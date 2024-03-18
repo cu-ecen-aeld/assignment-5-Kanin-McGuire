a92f4f9ce2d627a54407fbf3a06b50b4b1f2bae8
Kanin-McGuire <kamc9940@colorado.edu>
2024-03-17 22:24:11 -0600

    A8

--- base_external/package/aesdchar_driver/aesdchar_driver.mk
+++ base_external/package/aesdchar_driver/aesdchar_driver.mk
@@ -19,7 +19,6 @@ AESDCHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
 define AESDCHAR_DRIVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8-buildroot/drivertest.sh $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(kernel-module))
